e7089000bacd50bf48ecf94a244eb6a3e19e6b34
Mohamad Abdelhady <mohamad@MoHady.MoHady>
2021-08-18 18:21:00 +0200

    second commit

---
+++ resources/views/auth/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+</head>
+<body>
+<nav class="navbar" id="nav-bar">
+    <div class="container-fluid">
+        <div class="navbar-brand" id="logo-img"></div>
+    </div>
+</nav>
+</body>
+</html>
+
