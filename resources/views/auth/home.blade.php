1e050d2cb9e6d0680ce4641f568ee5c6ee2607eb
Mohamad Abdelhady <mohamad@MoHady.MoHady>
2021-08-18 19:41:59 +0200

    added a jumbtron to homw page and play store badge

--- resources/views/auth/home.blade.php
+++ resources/views/auth/home.blade.php
@@ -14,6 +14,19 @@
         <div class="navbar-brand" id="logo-img"></div>
     </div>
 </nav>
+<div class="jumbotron">
+    <h1 class="display-4">Welcome</h1>
+    <p class="lead">Intellect is subscription based service for reading books.</p>
+    <hr class="my-4">
+    <p>Subscribe now and enjoy 30 days free trial.</p>
+    <p class="lead">
+        <a class="btn btn-lg" style="background-color: #18181f; color: #f5eee2;" href="#" role="button">Subscribe</a>
+    </p>
+</div>
+<div align="center" class="">
+    <a href="#"><img src="/images/google-play-icon.png"></a>
+
+</div>
 </body>
 </html>
 
