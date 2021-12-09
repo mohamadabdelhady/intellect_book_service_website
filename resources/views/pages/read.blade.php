004b0d2f8beb432927abd987b40015315a74c1b9
Mohamad Abdelhady <mohamad@MoHady.MoHady>
2021-12-09 23:16:53 +0200

    added for reading the books

---
+++ resources/views/pages/read.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+</head>
+<body>
+@include('pages.nav-bar')
+<div class="container">
+
+
+    <div class="mt-5 ml-3"><hr>all copyright reserved for intellect book service&copy; 2021</div>
+</div>
+</body>
+</html>
