53dadfa0b98c8927cd83bd3b08b3a96a89126f4d
mohamadabdelhady <mohamadhady010@gmail.com>
2026-01-22 21:35:45 +0200

    added dashboard side nav, alyout for dashboard pages and controller and routes for every dashboard section as well as roles

--- resources/views/Dashboard/components/side_nav.blade.php
+++ resources/views/Dashboard/components/side_nav.blade.php
@@ -1,9 +1,14 @@
+<?php
+use App\Enums\AdminRole;
+?>
 <div class="side-nav-links" id="side-nav-links">
     <a href="{{ route('admin.dashboard') }}" class="side-nav-link">Dashboard</a>
+    @if (auth('admin')->user()->hasRole(AdminRole::ADMIN_SUPER))
     <a href="{{ route('admin.admins.index') }}" class="side-nav-link">Admins</a>
+    <a href="{{ route('admin.users.index') }}" class="side-nav-link">Users</a>
+    @endif
     <a href="{{ route('admin.books.index') }}" class="side-nav-link">Books</a>
     <a href="{{ route('admin.authors.index') }}" class="side-nav-link">Authors</a>
     <!-- <a href="" class="side-nav-link">Categories</a> -->
-    <a href="{{ route('admin.users.index') }}" class="side-nav-link">Users</a>
     <a href="{{ route('admin.reviews.index') }}" class="side-nav-link">Reviews</a>
 </div>
